a6464710d973de786796cf762aea2f2e43bef2a8
covxx <covenant123dx@gmail.com>
2015-03-01 23:48:32 -0500

    Small Changes - Check change log

--- start.rb
+++ start.rb
@@ -1,26 +1,30 @@
 #--------------------
-#BRIOS PRE-ALPHA 0.0.1 
+#BRIOS PRE-ALPHA 0.0.2
 #@COVXX
 #--------------------
-def boot
+#Any thing that needs to be required 
 require 'io/console'
 require 'FileUtils'
-#File for saving things
+require 'bcrypt' 
+
+
+def boot
 Print "Welcome to BRIOS..."
 Print "Please wait..."
-if #{thing} == 1
+if #{frun} == 1
   #This needs to call the 'desktop'
-elsif #{thing} == 0
+elsif #{frun} == 0 #Starts the first run process 
   Print "Lets get started"
-  Print "What do you want your username to be?: "
+  Print "What do you want your user name to be?: "
   user_name = gets.chomp
   Print "Choice a password: "
-  user_pass = gets.chomp
+  user_pass = gets.chomp #this needs to be encrypted  
   Print "Confirm your password: "
   userpass_confirm = gets.chomp
     if 
-      #{user_pass} == #{userpass_confirm}
-        Print "Password correct"
+      user_pass == userpass_confirm
+        Print "Password correct!"
         Print "One moment please.."
       #File.open("FR.txt, OPITON") {|f| f.write("1")}
 end
+
